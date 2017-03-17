980e45bfc4a3572dc0c500dcc266f8cea2b41b17
zhangzheng1993 <2296169562@qq.com>
2017-03-17 22:46:07 +0800

    origin benchmark which evaluate at sequences mentioned in cvpr2013

---
+++ util/checkResult.m
@@ -0,0 +1,25 @@
+function bfail=checkResult(results, subAnno)
+% validate the results
+% 
+% Some trackers may fail on some sequences so before evaulating the results
+% we have to check them.
+
+bfail=0;
+if isempty(results)
+    disp(['Empty all'])
+    bfail=1;
+else
+    bfail=1;
+    for i=1:length(results) 
+        if isempty(results{i}.res)
+            disp(['Empty sub ' num2str(i)])
+            bfail=1;
+        else
+            if size(results{i}.res,1)<size(subAnno{i},1)
+                disp(['Size sub ' num2str(i)])
+                bfail=1;
+            end
+            
+        end
+    end    
+end
